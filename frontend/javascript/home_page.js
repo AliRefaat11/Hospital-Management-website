66368c40f53e3e92281f0117e3219460cda5bb0e
AliRefaat11 <ali2303933@miuegypt.edu.eg>
2025-04-07 04:12:19 +0200

    profile page

--- frontend/javascript/home_page.js
+++
@@ -1,7 +0,0 @@
-// Toggle navigation menu on mobile
-const navToggle = document.getElementById('navToggle');
-const navLinks = document.getElementById('navLinks');
-
-navToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
---
+++ frontend/javascript/profile_page.js
@@ -0,0 +1,5 @@
+// Example: A simple greeting when the page loads
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to HealthyCare's profile page!");
+  });
+  
