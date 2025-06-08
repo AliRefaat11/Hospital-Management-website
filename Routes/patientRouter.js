4007d7133b0090bb2890830a7cc6c849a08ae8ae
Youssefnasr1 <youssefahmednasr000@gmail.com>
2025-06-08 21:56:28 +0300

    nasr

--- Routes/patientRouter.js
+++ Routes/patientRouter.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../Controllers/patientController');
 const authMiddleware = require('../middleware/auth');
-router.get('/', authMiddleware, patientController.getAllPatients); // List all patients
-router.get('/:id', authMiddleware, patientController.getPatientById); // Get a specific patient by ID
-router.post('/', authMiddleware, patientController.addPatient); // Add a new patient
-router.put('/:id', authMiddleware, patientController.updatePatient); // Update a patient's information
-router.delete('/:id', authMiddleware, patientController.deletePatient); // Delete a patient by ID
+router.get('/', authMiddleware, patientController.getAllPatients); 
+router.get('/:id', authMiddleware, patientController.getPatientById); 
+router.post('/', authMiddleware, patientController.addPatient); 
+router.put('/:id', authMiddleware, patientController.updatePatient); 
+router.delete('/:id', authMiddleware, patientController.deletePatient); 
 const express = require("express");
 const PatRouter = express.Router();
 const PatController = require("../Controllers/patientController");
