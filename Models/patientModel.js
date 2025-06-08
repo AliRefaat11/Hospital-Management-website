c091685882ae146e9538e38d0b80d2ff4d92be4c
AliRefaat11 <ali2303933@miuegypt.edu.eg>
2025-06-08 20:22:24 +0200

    fixing some error

--- Models/patientModel.js
+++ Models/patientModel.js
@@ -6,15 +6,5 @@ const patientSchema = new mongoose.Schema(
     }
 );
 
-class PatientModel {
-    constructor() {
-        mongoose.connect();
-        this.PatMod = mongoose.model('Patient', patientSchema);
-    }
-    async getAllPatients() {
-        let data = await this.PatMod.find({});
-        return JSON.stringify(data);
-    }
-}
-
-module.exports = PatientModel;
+const Patient = mongoose.model("Patient",patientSchema);
+module.exports = Patient;
