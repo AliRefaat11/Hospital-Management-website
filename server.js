d4594067bf959a7c62a18d332fb5f15471925d76
mohameddd2 <mohamedadelrizk8@gmail.com>
2025-06-10 20:13:25 +0300

    changes

--- server.js
+++ server.js
@@ -13,10 +13,18 @@ const MedRouter = require('./Routes/medicalreportRouter');
 const TreatRouter = require('./Routes/treatmentplanRouter');
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Views'));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use(express.static("./frontend"));
 app.use('/User', UserRouter);
